bde391c21f3e6a8159f5f581411a7cd5abe4caea
akselvazquez <damm084@tutanota.com>
2023-12-15 01:02:17 -0600

    Delete unnecesary dependencies

--- src/test/java/com/rest/api/user/AuthControllerTest.java
+++ src/test/java/com/rest/api/user/AuthControllerTest.java
@@ -3,15 +3,12 @@ package com.rest.api.user;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.json.MappingJacksonValue;
 
